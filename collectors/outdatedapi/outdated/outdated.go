cdd6646b2fba363f7a0cd077b273cd579ffa1cf8
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-08-25 03:24:56 +0600

    Create outdated.go

---
+++ collectors/outdatedapi/outdated/outdated.go
@@ -0,0 +1,20 @@
+package outdated
+
+import (
+	"fmt"
+)
+
+// ValidateOutdatedAPI validate outdated data is complete
+func ValidateOutdatedAPI(K8sapis map[string]map[string]map[string]string) (map[string]map[string]map[string]string, error) {
+	for _, ka := range K8sapis {
+		if len(ka) == 0 {
+			return nil, fmt.Errorf("failed to get outdated API missing Version or Kind or Group")
+		}
+		for _, pa := range ka {
+			if len(pa) == 0 {
+				return nil, fmt.Errorf("failed to get outdated API missing Version or Kind or Group")
+			}
+		}
+	}
+	return K8sapis, nil
+}
