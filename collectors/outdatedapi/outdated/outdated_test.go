da963e1490c3ab457d6ff159fcb43c3380862292
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-08-25 03:24:18 +0600

    Create outdated_test.go

---
+++ collectors/outdatedapi/outdated/outdated_test.go
@@ -0,0 +1,35 @@
+package outdated
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidateOutdatedAPI(t *testing.T) {
+	tests := []struct {
+		name     string
+		dataPath string
+		hasErr   bool
+	}{
+		{name: "complete outdated api data", dataPath: "./testdata/fixture/outdated_api.json", hasErr: false},
+		{name: "missing all outdated api data", dataPath: "./testdata/fixture/outdated_api_missing_all.json", hasErr: true},
+		{name: "missing partial outdated api data", dataPath: "./testdata/fixture/outdated_api_missing_partial.json", hasErr: true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fdata, err := os.ReadFile(tt.dataPath)
+			assert.NoError(t, err)
+			var outdatedAPI map[string]map[string]map[string]string
+			err = json.Unmarshal(fdata, &outdatedAPI)
+			assert.NoError(t, err)
+			_, err = ValidateOutdatedAPI(outdatedAPI)
+			if tt.hasErr {
+				assert.Error(t, err)
+			}
+		})
+	}
+}
